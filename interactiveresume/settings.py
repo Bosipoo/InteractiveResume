e893b16a9afcade817e57c4d305140025e9f52ef
BOSIPO <BOSIPO@LAPTOP-F66G2VEV>
2021-08-20 20:36:46 +0100

    allowedHost1

--- interactiveresume/settings.py
+++ interactiveresume/settings.py
@@ -4,18 +4,19 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-a%-@$h=ej8&#lp)$*oy043($(uyvz9@@s@%n%xk)ru9-s11lp3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://still-dusk-69333.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['still-dusk-69333.herokuapp.com','127.0.0.1']
 
+# ALLOWED_HOSTS = [
+#     '0.0.0.0'
+#     'the-yogify.herokuapp.com',
+#     '127.0.0.1'
+# ]
 
 # Application definition
 
